7c780843abec62cc78a6fa3eda24eeffb94d3d0d
JoshiProjects <josereinaldosoares94@gmail.com>
2025-02-10 00:01:13 -0300

    Updates

---
+++ precache-manifest.daaeedfe3a247b3a36862ea309535710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcdfb2cefd88f5b1125ea2f4c0068cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d566267894b60bd243b1",
+    "url": "/static/css/main.ece14ce2.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "d566267894b60bd243b1",
+    "url": "/static/js/main.9ae516ed.chunk.js"
+  },
+  {
+    "revision": "8a5626378c6138c21833",
+    "url": "/static/js/runtime~main.cef9ab07.js"
+  },
+  {
+    "revision": "c9166464b1db95fc3cdf9b50fc7f98e2",
+    "url": "/static/media/RobotoMono-Light.c9166464.ttf"
+  }
+]);
