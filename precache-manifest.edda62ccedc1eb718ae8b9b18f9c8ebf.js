f3bee63aac7097f2981eae9ce393db882ed7b6fb
JoshiProjects <josereinaldosoares94@gmail.com>
2025-02-10 00:22:59 -0300

    Updates

---
+++ precache-manifest.edda62ccedc1eb718ae8b9b18f9c8ebf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9e3e1e6a8479673271eec3a5b305e2",
+    "url": "/calculadora-react/index.html"
+  },
+  {
+    "revision": "a6e3925d51c4d169857f",
+    "url": "/calculadora-react/static/css/main.b404c62a.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/calculadora-react/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/calculadora-react/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "a6e3925d51c4d169857f",
+    "url": "/calculadora-react/static/js/main.9ae516ed.chunk.js"
+  },
+  {
+    "revision": "fda9880910422eb318e1",
+    "url": "/calculadora-react/static/js/runtime~main.7c8350ab.js"
+  },
+  {
+    "revision": "c9166464b1db95fc3cdf9b50fc7f98e2",
+    "url": "/calculadora-react/static/media/RobotoMono-Light.c9166464.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.daaeedfe3a247b3a36862ea309535710.js"
+  "/calculadora-react/precache-manifest.edda62ccedc1eb718ae8b9b18f9c8ebf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calculadora-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
